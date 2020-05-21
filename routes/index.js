1a6a1c8bed4d556967fc26b0eea9e81e4c934096
jorgerodriguezdelafuente <jorge.delafuente@yudonpay.com>
2020-05-21 17:47:44 +0200

    global

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log(env)
+
   console.log(env.MODE)
   res.render('index', { title: 'Express', envMode: (env.MODE == "LOCAL") ? "LOCALHOST" : "ONLINE" });
 });

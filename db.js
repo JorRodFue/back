684cd2460c85ffe3518a96b5fe4d986b9a04ecb3
jorgerodriguezdelafuente <jorge.delafuente@yudonpay.com>
2020-05-21 17:31:50 +0200

    database

--- db.js
+++ db.js
@@ -11,7 +11,16 @@ console.log(env.mode);
 // console.log("evn.hostname = ", env.HOSTNAME)
 // console.log("password", env.PASSWORDDB)
 
-console.log(`HOST : ${env.HOSTNAME}, \n USERDB : ${env.USERDB}, \n PASSWORDB : ${env.PASSWORDDB}`)
+
+const database = {
+  host: localhost ? env.HOSTNAMELOCAL : env.HOSTNAME,
+  user: localhost ? env.USERDBLOCAL : env.USERDB,
+  password: localhost ? env.PASSWORDDLOCAL : env.PASSWORDDB,
+  port: env.PORTDB || 3306,
+  database: localhost ? env.databaseLOCAL : env.DATABASE
+}
+
+console.log(database)
 const db = mysql.createPool(
 
   {
